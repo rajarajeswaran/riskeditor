48f1bda2406d7e9b8e280e838cfad96ccd65e1ca
Raja Anandarajan <AnandarR@globaldev.local>
2017-09-19 14:22:46 +0100

    Nav bar added with bs4 directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public isCollapsed = true;
+
   title = 'Risk Editor';
   dynamicTabs:Array<string> = ['Dynamic1','Dynamic2'];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import icons from 'glyphicons'
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
+
+
 
 import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component'
@@ -12,6 +16,7 @@ import {HomeComponent} from './home/home.component'
     HomeComponent
   ],
   imports: [
+    AngularFontAwesomeModule,
     BrowserModule,
     NgbModule.forRoot(),
   ],
@@ -19,6 +24,7 @@ import {HomeComponent} from './home/home.component'
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
 
 
 
