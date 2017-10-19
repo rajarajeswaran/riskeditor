d9fb2e0b44312280c72f3b990adf8bc738e5867c
Raja Anandarajan <AnandarR@globaldev.local>
2017-10-19 17:14:38 +0100

    - Risk model updated

--- src/app/app-state/risk/risk.model.ts
+++ src/app/app-state/risk/risk.model.ts
@@ -1,4 +1,81 @@
 export interface Risk {
-  policyId : string;
-  description: string;
+  PolicyId : string;
+  Description: string;
+  GUID: string;
+  Insured: Insured;
+  AnalysisCodes: AnalysisCode[];
+  AccountYear:number;
+  Currency:string;
+  Underwriter: Underwriter;
+  Domicile:string;
+  PolicyStatus:string;
 }
+
+export interface Insured{
+  InsuredId: number;
+  InsuredName: string;
+  InsuredType: string
+}
+
+
+export interface AnalysisCode{
+  Code: string;
+  Type: string;
+  Description: string;
+}
+
+
+/*
+{
+				"Code": "LON",
+				"Type": "ORIGOFF",
+				"Description": "London"
+}
+
+{
+				"Code": "LON",
+				"Type": "INTROFFICE",
+				"Description": "London"
+}
+{
+				"Code": "NA",
+				"Type": "NONLONBKR",
+				"Description": "Not Applicable"
+}
+{
+				"Code": "UK",
+				"Type": "DOMICILE",
+				"Description": "UNITED KINGDOM"
+}
+{
+				"Code": "SILENT",
+				"Type": "CYBER",
+				"Description": "Silent"
+}
+ */
+export interface Underwriter{
+  Code:  string;
+  Name:  string;
+}
+
+export interface Broker {
+  BrokerPseudonym: string;
+  BrokerSequenceId: number;
+  BrokerNumber:  number;
+  BrokerType: string;
+  BrokerContactName: string;
+  BrokerContactEmail: string;
+  BrokerContactTelephoneNumber: string;
+}
+
+/*
+{
+				"BrokerPseudonym": "LMN",
+				"BrokerSequenceId": 991,
+				"BrokerNumber": "1235",
+				"BrokerType": null,
+				"BrokerContactName": "Ciara Appleford",
+				"BrokerContactEmail": "Ciara.Appleford@mena.lockton.com",
+				"BrokerContactTelephoneNumber": null
+}
+ */
--- src/app/app-state/risk/risk.reducer.ts
+++ src/app/app-state/risk/risk.reducer.ts
@@ -4,7 +4,7 @@ import { memoize } from 'lodash';
 
 
 import { RiskState } from './risk.state';
-import {Risk} from './risk.model'
+import {Risk,AnalysisCode,Underwriter} from './risk.model'
 import * as RiskActions from './risk.actions';
 
 const initialState: RiskState = {
@@ -17,12 +17,25 @@ function(state: RiskState = initialState, action: Action): RiskState {
     case RiskActions.SET_CURRENT_RISK:
     return Object.assign({}, state);
     case RiskActions.ADD_NEW_RISK:
-    var mutation = state.risks.concat([{description:'new', policyId:(state.risks.length?state.risks.length+1:1).toString()}]);
+    var risk :Risk = {
+      PolicyId : '',
+      Description: '',
+      GUID:  '',
+      Insured:  {InsuredId: 0,        InsuredName: '',        InsuredType: ''},
+      AnalysisCodes:  new Array<AnalysisCode>(),
+      AccountYear:0,
+      Currency:'',
+      Underwriter : { Code:  '',        Name:  ''},
+      Domicile:'',
+      PolicyStatus:'',
+    };
+    risk.PolicyId =(state.risks.length?state.risks.length+1:1).toString();
+    var mutation = state.risks.concat([risk]);
     var newState =  Object.assign({}, state, {risks:mutation});
     return newState;
     case RiskActions.EDIT_RISK:
     var newState1 =  Object.assign({}, state);
-    newState1.risks[0].description = "EDIT"
+    newState1.risks[0].Description = "EDIT"
     return newState1;
     default:
       return state;
@@ -31,7 +44,7 @@ function(state: RiskState = initialState, action: Action): RiskState {
 
 export const getRiskState = (state): RiskState => state.RiskState;
 export const getRiskById  =   createSelector(getRiskState, rs => memoize(index => rs.risks[index]));
-export const getRiskIds  =   createSelector(getRiskState, rs => rs.risks.map(r=>r.policyId));
+export const getRiskIds  =   createSelector(getRiskState, rs => rs.risks.map(r=>r.PolicyId));
 
 
 
